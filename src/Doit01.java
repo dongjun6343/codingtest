1c3f029368038e787bf421510ae33c48dd5b7e81
dongjun6343 <dongjun6343@naver.com>
2022-07-15 14:32:51 +0900

    #add  숫자의 합 구하기

--- src/Doit01.java
+++ src/Doit01.java
@@ -1,49 +1,38 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * 백준 : 2750번
- * 수 정렬하기
- * N개의 수가 주어졌을 때 이름 오름차순 정렬하는 프로그램을 작성하시오.
+ *  백준 11720번
  *
- * 1번쨰 줄에 수의 개수 N(1 <= N <= 1,000,000) , 2번째 줄부터는 N개의 줄의 개수가 주어진다.
- * 이 수는 절댓값이 1,000,000보다 작거나 같은 정수다.
- * 수는 중복되지 않는다.
+ *  숫자의 합 구하기
+ *  N개의 숫자가 공백없이 써 있다. 이 숫자를 모두 합해 출력하는 프로그램을 작성하시오.
  *
- * --------              --------
- * 예제 입력               예제 출력
- * --------              --------
- * 5                        1
- * 5                        2
- * 2                        3
- * 3                        4
- * 4                        5
- * 1
+ *  입력
+ *  1번째 줄에 숫자의 개수 N(1<= N <= 100) , 2번째 줄에 숫자 N개가 공백 없이 주어진다.
  *
+ *  출력
+ *  입력으로 주어진 숫자 N개의 합을 출력한다.
  *
+ *  -------                            -------
+ *  예제입력                               출력
+ *  -------                            -------
+ *                                        1
+ *  1 // 숫자의 개수
+ *  1 // 공백 없이 주어진 N개의 숫자
  */
 
 public class Doit01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int inputValue = sc.nextInt();
-        // 1. 수의 개수값.
-        // 배열생성
-        int[] arr = new int[inputValue];
-    
-        // 입력한 값 배열에 담기
-        for (int i = 0; i<inputValue; i++){
-            arr[i] = sc.nextInt();
-        }
-        // Arrays.sort
-        // 정렬
-        Arrays.sort(arr);
+        // 숫자의 개숫
+        int inputLeng = sc.nextInt();
 
-        // 배열에 있는 값 하나씩 출력
-        for (int val: arr) {
-            System.out.println(val);
+        int sum = 0;
+        String inputValue = sc.next();
+        for (int i = 0; i<inputLeng; i++ ){
+            //아스키코드값 빼기. 48 , '0'
+            sum += inputValue.charAt(i)-'0';
         }
+        System.out.println(sum);
     }
 }
