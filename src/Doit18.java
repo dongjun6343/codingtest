0f3ba46bd5ea6fcf337064bda80975e372a53f16
dongjun6343 <dongjun6343@naver.com>
2022-09-15 23:17:40 +0900

    commit

---
+++ src/Doit18.java
@@ -0,0 +1,73 @@
+import java.util.Scanner;
+
+/**
+ *  백준 11399번
+ * ATM 인출시간 계산하기
+ *  [입력]
+ *
+ *  [출력]
+ *
+ *  ------              ------
+ *  입력값                출력값
+ *  ------              ------
+ *  5                    32
+ *  3 1 4 3 2
+ *  ---------------------------------------------------------------------------------------------
+ *  [HINT]
+ *
+ *  [CHECK]
+ *
+ */
+public class Doit18
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+
+        int[] A = new int[N];
+        int[] S = new int[N];  //합배열.
+
+
+        for(int i = 0 ;i< N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        // 삽입정렬
+        for(int i = 1; i < N; i++) {
+            int insert_point = i; // 1
+            int insert_value = A[i]; //1
+
+            for(int j = i - 1; j >= 0; j--) {
+                // 3       1
+                // 1       4
+                if(A[j] < A[i]) {
+                    insert_point = j + 1;
+                    break;
+                }
+                if(j == 0) {
+                    insert_point = 0;
+                }
+            }
+
+            for (int j = i; j > insert_point; j--) {
+                A[j] = A[j-1];
+            }
+            A[insert_point] = insert_value;
+        }
+
+        S[0] = A[0];
+        for(int i = 1; i < N; i++) { // 합배열.
+            S[i] = S[i-1] + A[i];
+        }
+
+        int sum = 0;
+        for(int i = 0; i < N; i++) { // 합배열 총합
+            sum = sum + S[i];
+        }
+        System.out.println(sum);
+
+    }
+}
+
+
---
+++ src/Doit19.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+// 퀵정렬
+// 피벗을 중심으로 계산
+// K번쨰에 있는 수를 구하는 프로그램.
+
+// 5 2				2
+// 4 1 2 3 5
+public class Doit19
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt(); // 피벗
+
+        int[] A = new int[N];
+
+        // 배열에 값 할당
+        for(int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        quickSort(A, 0, N-1, K-1);
+        System.out.println(A[K-1]);
+    }
+
+
+    // 퀵정렬
+    public static void quickSort(int[] A, int S, int E, int K) {
+        // K : 찾는 수
+        // p == k : 종료
+        // p < k : 왼쪽만 정렬
+        // p > k : 오른쪽만 정렬
+        if(S < E) {
+            int pivot = partition(A, S, E);
+            if(pivot == K) {
+                return;
+            } else if(K < pivot) {
+                quickSort(A, S, pivot-1 , K); //왼쪽만 정렬
+            } else {
+                quickSort(A, pivot+1, E, K); //오른쪽만 정렬
+            }
+        }
+
+    }
+
+    // 파티션
+    public static int partition(int[] A, int S, int E) {
+        int M = (S + E) / 2;
+        swap(A, S, M);
+        int pivot = A[S];
+        int i = S;
+        int j = E;
+        while(i < j) {
+            while(pivot < A[j]) {
+                j--;
+            }
+            while(i < j && pivot >= A[i]) {
+                i++;
+            }
+            swap(A, i, j);
+        }
+
+        A[S] = A[i];
+        A[i] = pivot;
+        return i;
+    }
+
+    // swap
+    public static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
+
+}
