86a0736dc9a8397a22da7914d4e0a198a6cbdf78
dongjun6343 <dongjun6343@naver.com>
2022-10-19 22:05:32 +0900

    슬라이딩 윈도우와 정렬

---
+++ src/doit_핵심유형/Doit_010.java
@@ -0,0 +1,18 @@
+package doit_핵심유형;
+
+
+// 11003번
+// 일정범위 안에서 최솟값을 구하는 문제
+// 슬라이딩 윈도우와 정렬을 사용하자!
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Doit_010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
