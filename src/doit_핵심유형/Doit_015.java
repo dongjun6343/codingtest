234c52adb3adf059f163b1209707c8a9a8a19c67
dongjun6343 <dongjun6343@naver.com>
2022-10-26 14:37:38 +0900

    입출력 효과적으로 변경

--- src/doit_핵심유형/Doit_015.java
+++ src/doit_핵심유형/Doit_015.java
@@ -1,5 +1,9 @@
 package doit_핵심유형;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -24,19 +28,28 @@ import java.util.Scanner;
 // 5
 public class Doit_015 {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        
+        // 입출력을 효율적으로 변경
+        //Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        // int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[] k = new int[n];
 
         for(int i = 0; i < n; i++){
-            k[i] = sc.nextInt();
+//            k[i] = sc.nextInt();
+            k[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(k);
 
         for (int val : k) {
-            System.out.println(val);
+            sb.append(val).append('\n');
         }
+
+        System.out.println(sb);
     }
 }
---
+++ src/doit_핵심유형/Doit_023.java
@@ -0,0 +1,22 @@
+package doit_핵심유형;
+
+import java.util.ArrayList;
+
+/**
+ * 연결 요소의 개수 구하기
+ * 11724번
+ *
+ * 깊이 우선 탐색
+ * DFS -> 후입선출 -> 스택
+ *
+ * 연결 요소 문제는 대표적인 DFS와 BFS문제이다.
+ */
+public class Doit_023 {
+    static ArrayList<Integer>[] A;
+    static boolean visited[];
+
+    public static void main(String[] args) {
+
+    }
+
+}
