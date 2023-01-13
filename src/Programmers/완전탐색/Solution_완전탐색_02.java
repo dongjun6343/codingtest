3c2cf7a83c5a31c906ab7a4855ef20d5b3b58835
dongjun6343 <dongjun6343@naver.com>
2023-01-13 16:32:43 +0900

    프로그래머스 문제풀이

--- src/Programmers/완전탐색/Solution_완전탐색_02.java
+++ src/Programmers/완전탐색/Solution_완전탐색_02.java
@@ -2,6 +2,7 @@ package Programmers.완전탐색;
 
 
 import java.util.HashSet;
+import java.util.Iterator;
 
 /**
  * [소수 찾기]
@@ -47,8 +48,45 @@ class Solution_완전탐색_02 {
         // 1. 모든 조합의 수를 만든다. 1, 7 , 17, 71
         numberMake("", numbers);
 
+        // 2. 소수의 개수만 count
+        Iterator<Integer> iter = numberSet.iterator();
+        while(iter.hasNext()){
+            int num = iter.next();
+            // 소수 찾기
+            if(isPrime(num)){
+                answer++;
+            }
+        }
 
         return answer;
+    }
+
+    /**
+     * 에라토스테네스의 체란 2-MaxNum까지 전부 배수인지 확인할 필요가 없다는 것을 증명한 수학적인 이론이다.
+     * 예를 들어 97이라는 숫자가 소수인지 아닌지를 확인하고 싶다면, 97이 2~96 전부의 배수인지 확인할 필요가 없고,
+     * 2~sqrt(96)의 배수인지만 확인하면 된다는 이론이다.
+     *
+     * 그렇기 때문에 isPrime 함수에서 전부를 보지 않고, sqrt한 값까지만 확인하고 비교한다.
+     * sqrt(제곱근) : 25 -> 5.0
+     *
+     */
+    private boolean isPrime(int num) {
+        // 0, 1 제외
+        // num => 7, 17, 71
+        if(num == 0 || num == 1){
+            return false;
+        }
+        // 에라토스테네스의 체의 limit 숫자 계산.
+        int limit = (int)Math.sqrt(num);
+
+        // limit까지 배수 여부 확인.
+        for(int i = 2; i <= limit; i++ ){
+            // 0이면 소수가 아니다.
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 
 
@@ -58,6 +96,11 @@ class Solution_완전탐색_02 {
         // set : 순서가 없고, 중복을 허용하지 않는다.
         if(!s.equals("")){
             numberSet.add(Integer.valueOf(s));
+        }
+
+        // 남은 숫자로 새로운 조합을 만듬.
+        for(int i = 0; i < numbers.length(); i++){
+            numberMake(s + numbers.charAt(i), numbers.substring(0, i) + numbers.substring(i + 1));
         }
     }
 }
