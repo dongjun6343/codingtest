51d3a7a70dc44eefb94c0d1d5b2d7b4c8dc0409d
dongjun6343 <dongjun6343@naver.com>
2023-01-18 17:44:23 +0900

    프로그래머스 문제풀이

--- src/Programmers/정렬/Solution_정렬_00.java
+++ src/Programmers/정렬/Solution_정렬_00.java
@@ -1,6 +1,9 @@
 package Programmers.정렬;
 
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 /**
  * [K번째수]
  * 문제 설명
@@ -33,13 +36,76 @@ public class Solution_정렬_00 {
         System.out.println(s.solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2,5,3},{4,4,1},{1,7,3}}));
     }
     public int[] solution(int[] array, int[][] commands) {
-        // 1. commands : 2,5 - 구간설정
-        // 2. 2~5 안에 있는 수 뽑은 후 정렬 => 2,3,5,6
+
+
         // 3. commands : 3 - 3번째 수 뽑기 => 5.
         // 4. 5 리턴.
 
+        //j -> 0 : 구간1
+        //j -> 1 : 구간2
+        //j -> 2 : 뽑는수
+        int start = 0;
+        int end = 0;
+        int choice = 0;
+        ArrayList<Integer> answerList = new ArrayList<>();
 
-        int[] answer = {};
+        for(int i = 0; i < commands.length; i++){
+            for(int j = 0; j < commands[i].length; j++){
+                if(j == 0){
+                    start = commands[i][j];
+                } else if(j == 1){
+                    end = commands[i][j];
+                } else if(j == 2){
+                    choice = commands[i][j];
+                }
+            }
+            // 1. commands : 2,5 - 구간설정
+            // int chk[] = new int[end-1];
+            ArrayList<Integer> arrchk = new ArrayList<>();
+
+            int cnt = 0;
+            // 4 4
+
+            for(int k = start-1; k < end; k++){
+                int arr = array[k];
+                //chk[cnt] = arr;
+                arrchk.add(arr);
+                cnt++;
+            }
+            // 2. 2~5 안에 있는 수 뽑은 후 정렬 => 2,3,5,6 오름차순.
+//            arrchk.sort((o1, o2) -> o1.compareTo(o2));
+            arrchk.sort(Comparator.naturalOrder());
+            System.out.println(arrchk);
+            // int pick = chk[choice-1];
+
+            answerList.add(arrchk.get(choice-1));
+        }
+
+        int[] answer = new int[answerList.size()];
+
+        for(int i = 0 ; i < answerList.size(); i++){
+            answer[i] = answerList.get(i).intValue();
+        }
+
         return answer;
     }
 }
+
+
+/**
+ * Arrays.copyOfRange 사용.
+ *
+ * class Solution {
+ *     public int[] solution(int[] array, int[][] commands) {
+ *         int[] answer = new int[commands.length];
+ *
+ *         for(int i=0; i<commands.length; i++){
+ *             int[] temp = Arrays.copyOfRange(array, commands[i][0]-1, commands[i][1]);
+ *             Arrays.sort(temp);
+ *             answer[i] = temp[commands[i][2]-1];
+ *         }
+ *
+ *         return answer;
+ *     }
+ * }
+ */
